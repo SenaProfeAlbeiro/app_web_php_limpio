0ad0919a067f1d292ec698f0c2d40c18880bdb47
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-25 10:05:50 -0500

    7. Manejo de Sesiones

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -1,7 +1,8 @@
 <?php
-class Dashboard{
-	public function main(){
-		$session = $_SESSION['session'];
-		require_once "views/roles/".$session."/".$session.".view.php";
+	class Dashboard{
+		public function main(){
+			$session = $_SESSION['session'];
+			require_once "views/roles/".$session."/".$session.".view.php";
+		}
 	}
-}
+?>
